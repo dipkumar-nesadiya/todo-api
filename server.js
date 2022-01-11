28dcf8014422981739454eba989f1ed09c1c9012
dipnesadiya123 <dipkumar.nesadiya@smartsensesolutions.com>
2022-01-11 11:22:52 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+let todoNextID = 1;
 
-const todos = [{
-    id: 1,
-    description: 'Go to movie',
-    completed: false
-}, {
-    id: 2,
-    description: 'Complete section 7 in node js course',
-    completed: false
-}, {
-    id: 3,
-    description: 'Learn basics of Express',
-    completed: true
-}];
+const todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Todo API Root!');
@@ -38,6 +30,18 @@ app.get('/todos/:id', (req, res) => {
     if (matched === false) {
         res.status(404).send();
     }
+});
+
+//Here we are adding todo task dynamically
+//Post contains data so we can pass todo data to it. It contains JSON data and server takes this data
+// and stores in array
+app.post('/todos', (req,res) => {
+    let body = req.body;
+
+    body.id = todoNextID++;
+    todos.push(body);
+
+    res.json(body);
 });
 
 app.listen(PORT, (req, res) => {
